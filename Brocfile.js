1f86d3adbc210187076375f71857c5eda6864ec0
Garrett Heinlen <heinleng@gmail.com>
2014-08-10 12:23:26 +1000

    Adding in localstorage adapter

--- Brocfile.js
+++ Brocfile.js
@@ -4,6 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+app.import("vendor/ember-localstorage-adapter/localstorage_adapter.js");
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
