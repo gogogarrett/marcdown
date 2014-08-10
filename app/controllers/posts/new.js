65fafcee04c9bda5d7dabfd295d44a6c4de99663
Garrett Heinlen <heinleng@gmail.com>
2014-08-10 13:20:22 +1000

    Hooking up new

--- app/controllers/posts/new.js
+++ app/controllers/posts/new.js
@@ -2,16 +2,32 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  resetForm: function() {
+    this.set("fields", {});
+    this.set("formErrors", {});
+  },
+
   actions: {
-    savePost: function() {
-      var post = this.store.createRecord('post', this.get("fields"));
-      var that = this;
+    save: function() {
+      var _that = this;
+      var post = this.store.createRecord("post", this.get("fields"));
 
-      post.save().then(function(post) {
-        that.transitionToRoute('posts');
+      post.save().then(function(){
+        _that.resetForm();
+        _that.transitionToRoute("posts.index");
+      }, function(post) {
+        post.deleteRecord();
+
+        _that.set("formErrors", {});
+        for (var key in post.errors) {
+          _that.set("formErrors." + key, true);
+        }
       });
+    },
 
-      this.set("fields", {});
+    cancel: function() {
+      this.resetForm();
+      this.transitionToRoute("posts.index");
     }
   }
 });
--- app/routes/posts/new.js
+++ app/routes/posts/new.js
@@ -3,5 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupController: function(controller) {
     controller.set("fields", {});
+    controller.set("formErrors", {});
   }
 });
