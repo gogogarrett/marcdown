34e604615d9bdefa3fd11e8f14df96a7e97d08c2
Garrett Heinlen <heinleng@gmail.com>
2014-08-11 20:38:31 +1000

    adding in a word count class

--- app/helpers/word-count.js
+++ app/helpers/word-count.js
@@ -1,11 +1,13 @@
 import counter from "../helpers/word-counter";
 
 export default = Ember.Handlebars.makeBoundHelper(function (markdown) {
+  if (typeof markdown === 'undefined') { return "0 words"; }
+
   if (/^\s*$/.test(markdown)) {
-    return '0 words';
+    return "0 words";
   }
 
   var count = counter(markdown || '');
-  return count + (count === 1 ? ' word' : ' words');
+  return count + (count === 1 ? " word" : " words");
 });
 
