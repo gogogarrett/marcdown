df3edf269a834617f37ba0575662ae7412f53f9d
Garrett Heinlen <heinleng@gmail.com>
2014-08-11 20:37:42 +1000

    fixing some syntax and making it not break if theres no input.

--- app/helpers/format-markdown.js
+++ app/helpers/format-markdown.js
@@ -1,5 +1,5 @@
 export default Ember.Handlebars.makeBoundHelper(function(input) {
-  if (typeof input == 'undefined')  return;
+  if (typeof input === 'undefined') { return; }
   var showdown = new Showdown.converter();
   return new Handlebars.SafeString(showdown.makeHtml(input));
 });
