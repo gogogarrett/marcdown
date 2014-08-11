d9f449ef45defec7d457df5c3b4a7d6e9e265db0
Garrett Heinlen <heinleng@gmail.com>
2014-08-11 20:37:26 +1000

    adding in moment and showdown.

--- Brocfile.js
+++ Brocfile.js
@@ -5,6 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 app.import("vendor/ember-localstorage-adapter/localstorage_adapter.js");
+app.import("vendor/moment/moment.js");
+app.import("vendor/showdown/src/showdown.js");
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
